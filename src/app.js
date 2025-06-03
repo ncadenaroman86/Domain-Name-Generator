a99a70a1a19a3deacc3d19e596b73aa6152d64e8
Nelson Cadena <32472985+ncadenaroman86@users.noreply.github.com>
2025-06-03 00:46:47 +0000

    Domain Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
 
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let ext of extensions) {
+        console.log(p + a + n + ext);
+      }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+      // Domain hack: if the noun ends with the same letters as a TLD
+      let hackExt = 'us';
+      if (n.endsWith(hackExt)) {
+        let hackedDomain = p + a + n.replace(hackExt, '') + '.' + hackExt;
+        console.log(hackedDomain);
+      }
+    }
+  }
+}
